53e0897e7550d7600c97105fa49b46aba79ef91b
Mark Szavin <szavinm@yahoo.com>
2020-10-02 15:00:25 +0200

    add form data procesing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
@@ -14,16 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  req.on("data", (data) => {
-    const parsed = data.toString("utf8").split("&");
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split("=");
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
-  res.send("account created!");
+  console.log(req.body), res.send("account created!");
 });
 
 app.listen(3000, () => {
