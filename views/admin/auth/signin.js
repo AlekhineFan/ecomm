97dd52b48f24ec481fe7b47a4caf13a4c89742b7
Mark Szavin <szavinm@yahoo.com>
2020-10-06 07:21:28 +0200

    relocate getError func

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,10 +1,13 @@
 const layout = require("../layout.js");
-module.exports = () => {
+const { getError } = require("../../helpers.js");
+module.exports = ({ errors }) => {
   return layout({
     content: `<div>
       <form method="post">
           <input name="email" placeholder="email" />
+          ${getError(errors, "email")}
           <input name="password" placeholder="password" />
+          ${getError(errors, "password")}
           <button>Sign in</button>
         </form>
      </div>
