41f8c1eaac77df86c5710484370234d6078330ea
Mark Szavin <szavinm@yahoo.com>
2020-10-06 07:37:22 +0200

    add middleware to reach public folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
