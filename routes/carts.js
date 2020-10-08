fba64502f8d23e79e34dc44eb2d9f4375d463ff6
Mark Szavin <szavinm@yahoo.com>
2020-10-08 17:44:14 +0200

    add routing for managing carts

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cartsRepository = require("../repositories/carts");
+const productsRepository = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
+
 const router = express.Router();
 
 router.post("/cart/products", async (req, res) => {
@@ -21,7 +24,30 @@ router.post("/cart/products", async (req, res) => {
   await cartsRepository.update(cart.id, {
     items: cart.items,
   });
-  res.send("product added to cart");
+  res.redirect("/cart");
+});
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+  const cart = await cartsRepository.getOne(req.session.cartId);
+  for (let item of cart.items) {
+    const product = await productsRepository.getOne(item.id);
+    item.product = product;
+  }
+  res.send(cartShowTemplate({ items: cart.items }));
+});
+
+router.post("/cart/products/delete", async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await cartsRepository.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+
+  await cartsRepository.update(req.session.cartId, { items });
+
+  res.redirect("/cart");
 });
 
 module.exports = router;
