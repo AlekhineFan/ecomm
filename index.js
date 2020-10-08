558fa611f1b245ce946b71dd2ce0e0b42ec2d043
Mark Szavin <szavinm@yahoo.com>
2020-10-08 15:58:48 +0200

    add cartsRouter

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,6 +19,7 @@ app.use(
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("listening...");
